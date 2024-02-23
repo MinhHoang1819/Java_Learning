719e3a3667fd5346f5becb4b141c56dac384ff92
MinhHoang1819 <115516411+MinhHoang1819@users.noreply.github.com>
2024-02-23 15:28:47 +0700

    BT

---
+++ javacore/src/main/java/mh/javacore/Javacore.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mh.javacore;
+
+/**
+ *
+ * @author salen
+ */
+public class Javacore {
+
+    public static void main(String[] args) {
+        int a= 5;
+        System.out.println("giá trị khởi tạo: "+a);
+    }
+}
